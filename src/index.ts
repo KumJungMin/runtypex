b1cbdaed7cfab71452567b5bbf3b5db305a39a9f
KumJungMin <today.as.fresh@gmail.com>
2025-10-17 10:13:18 +0900

    feat: update version to 0.1.2 and improve module exports in package.json; clean up import paths in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { makeValidate, makeAssert, type ValidateFn } from "./runtime/validate.js";
-export { default as vitePlugin } from "./transformer/vite-plugin.js";
-export { default as tsTransformer } from "./transformer/ts-transformer.js";
+export { makeValidate, makeAssert, type ValidateFn } from "./runtime/validate"
+export { default as vitePlugin } from "./transformer/vite-plugin"
+export { default as tsTransformer } from "./transformer/ts-transformer"
